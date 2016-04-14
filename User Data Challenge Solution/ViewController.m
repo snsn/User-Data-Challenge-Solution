82c39b3b4f9a1c095c56af349cb7efbd0b6904e5
Donghyeon lim <gmaeodeod@gmail.com>
2016-04-15 02:00:34 +0900

    Get users data and logging user object

--- User Data Challenge Solution/ViewController.m
+++ User Data Challenge Solution/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "DHUserData.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.users = [[NSMutableArray alloc] init];
+    
+    for (NSDictionary *userData in [DHUserData users])
+    {
+        NSLog(@"returned data is %@", userData);
+    }
 }
 
 - (void)didReceiveMemoryWarning {
