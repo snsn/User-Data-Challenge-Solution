fd56f4610932537480ed73f8babf00a5a2379365
Donghyeon lim <gmaeodeod@gmail.com>
2016-04-15 01:32:51 +0900

    Add user data class

---
+++ DHUserData.m
@@ -0,0 +1,13 @@
+//
+//  DHUserData.m
+//  User Data Challenge Solution
+//
+//  Created by Donghyeon Lim on 4/15/16.
+//  Copyright © 2016 Donghyeon Lim. All rights reserved.
+//
+
+#import "DHUserData.h"
+
+@implementation DHUserData
+
+@end
