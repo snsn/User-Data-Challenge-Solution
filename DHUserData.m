370db111b5d63c49ddc1f8a217fc04680d0f17cf
Donghyeon lim <gmaeodeod@gmail.com>
2016-04-15 01:50:12 +0900

    Add user data

--- DHUserData.m
+++ DHUserData.m
@@ -7,6 +7,7 @@
 //
 
 #import "DHUserData.h"
+#import "UIKit/UIkit.h"
 
 @implementation DHUserData
 
@@ -14,7 +15,40 @@
 {
     NSMutableArray *userList = [@[] mutableCopy];
     
-    return userList;
+    UIImage *firstUserImage = [UIImage imageNamed:@"person1.jpeg"];
+    NSDictionary *firstUser = @{USER_NAME: @"철수",
+                                EMAIL: @"person1@gmail.com",
+                                PASSWORD: @"password1",
+                                AGE: @17,
+                                PROFILE_PICTURE: firstUserImage};
+    
+    UIImage *secondUserImage = [UIImage imageNamed:@"person2.jpeg"];
+    NSDictionary *secondUser = @{USER_NAME: @"영희",
+                                EMAIL: @"person2@gmail.com",
+                                PASSWORD: @"password2",
+                                AGE: @18,
+                                PROFILE_PICTURE: secondUserImage};
+    
+    UIImage *thirdUserImage = [UIImage imageNamed:@"person3.jpg"];
+    NSDictionary *thirdUser = @{USER_NAME: @"지수",
+                                EMAIL: @"person3@gmail.com",
+                                PASSWORD: @"password3",
+                                AGE: @27,
+                                PROFILE_PICTURE: thirdUserImage};
+    
+    UIImage *fourthtUserImage = [UIImage imageNamed:@"person4.jpeg"];
+    NSDictionary *fourthUser = @{USER_NAME: @"민수",
+                                EMAIL: @"person4@gmail.com",
+                                PASSWORD: @"password4",
+                                AGE: @37,
+                                PROFILE_PICTURE: fourthtUserImage};
+    
+    [userList addObject:firstUser];
+    [userList addObject:secondUser];
+    [userList addObject:thirdUser];
+    [userList addObject:fourthUser];
+    
+    return [userList copy];
 }
 
 @end
