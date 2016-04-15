f4d766c83313bf6c16743e0c7f3eebd05d8e0362
Donghyeon lim <gmaeodeod@gmail.com>
2016-04-15 11:51:08 +0900

    Create user object class

---
+++ User Data Challenge Solution/DHUserObject.h
@@ -0,0 +1,13 @@
+//
+//  DHUserObject.h
+//  User Data Challenge Solution
+//
+//  Created by Donghyeon Lim on 4/15/16.
+//  Copyright © 2016 Donghyeon Lim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DHUserObject : NSObject
+
+@end
